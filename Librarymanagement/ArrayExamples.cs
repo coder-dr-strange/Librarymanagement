de848addec3d0ef7f9582e4336e368f5c093f7f6
Coder-dr-strange <askdrchitrangda@gmail.com>
2025-05-02 10:48:25 +0530

    outputs are not getting printed

--- Librarymanagement/ArrayExamples.cs
+++ Librarymanagement/ArrayExamples.cs
@@ -18,7 +18,7 @@ namespace Librarymanagement
             int sum = integers[0] + integers[1] + integers[2] + integers[3] + integers[4];
             Console.WriteLine("3. Calculate the sum of all elements in an array: " + sum);
 
-            int Sum = integers.Sum();
+            *int Sum = integers.Sum();
             Console.WriteLine("3. (Variation) Value of sum can be " + integers.Sum());
             Console.WriteLine($"3. (variation) Value of sum : {Sum}");
 
@@ -59,7 +59,7 @@ namespace Librarymanagement
             bool found = true;
 
             foreach (int value in max)
-            { if (x==32)
+            { if (x == 32)
                 {
                     found = true;
                     break;
@@ -69,7 +69,8 @@ namespace Librarymanagement
             }
 
             //Input: mat[][] = [[14, 34, 23, 95, 43, 28]], x = 55
-            int[,] mat = { {14,34,23,95,28} };
+
+            int[] mat = { 14, 34, 23, 95, 28 };
             int X = 55;
             bool notfound = false;
             foreach (int value in mat)
@@ -80,9 +81,49 @@ namespace Librarymanagement
                     break;
                 }
             }
-            Console.WriteLine("Example 2 value of X "+notfound);
+            Console.WriteLine("Example 2 value of X " + notfound);
+
+            // Input: MAT[][] = [[87, 9, 99], [101, 3, 111]], xx = 101
+
+            int[,] MAT =
+            { {87,9,99 },
+                {101,3,111 }
+            };
+            int xx = 101;
+            bool xxfound = true;
+            foreach (int value in MAT)
+            {
+                if (xx == 101)
+                { xxfound = true;
+                    break;
+                }
+                Console.WriteLine("example 3 value of xx " + xxfound);
+            }
+
+            /* Input:
+            N = 7
+            Arr[] = { 1, 2, 3, 2, 3, 1, 3 }
+            Output: 3
+            Explaination: 3 occurs three times.*/
+
+            int[] Arr = new int[7] { 1, 2, 3, 2, 3, 1, 3 };
+           int[] repeatationof3 = Arr.Where(w => 3 == 0).ToArray();
+            int rep3 = Arr.Length;
+            Console.WriteLine(string.Join(","+Arr));
+
+            int arr = 3;
+            foreach (int value in Arr)
+            {
+                if (x == 3)
+                {
+                    
+                    return;
+                }
+                Console.WriteLine($"Repeatatn of 3 is {rep3} times");
+            }
 
 
-        }
+
+        } 
     }
 } 
